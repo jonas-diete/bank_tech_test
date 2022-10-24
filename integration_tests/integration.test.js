1aa6447d565c546d3ce894e726e9cd5163bc706e
Jonas Diete <jonas.diete@gmail.com>
2022-10-24 13:17:00 +0100

    multiple withdrawing and depositing with correct statement printed

--- integration_tests/integration.test.js
+++ integration_tests/integration.test.js
@@ -24,14 +24,24 @@ describe('Transactions', () => {
     const statement = new Statement;
     const transactions = new Transactions(account, statement);
     transactions.deposit(1000, '10/01/2023');
-    expect(statement.printStatement()).toBe('date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00')
+    expect(statement.printStatement()).toBe('date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00');
   });
 
   it('adds correct statement line after withdrawing', () => {
     const account = new Account;
     const statement = new Statement;
     const transactions = new Transactions(account, statement);
-    transactions.withdraw(500, '10/01/2023');
-    expect(statement.printStatement()).toBe('date || credit || debit || balance\n10/01/2023 || || 500.00 || -500.00')
+    transactions.withdraw(500, '14/01/2023');
+    expect(statement.printStatement()).toBe('date || credit || debit || balance\n14/01/2023 || || 500.00 || -500.00');
+  });
+
+  it('prints the whole correct statement after depositing and withdrawing a few times', () => {
+    const account = new Account;
+    const statement = new Statement;
+    const transactions = new Transactions(account, statement);
+    transactions.deposit(1000, '10/01/2023');
+    transactions.deposit(2000, '13/01/2023');
+    transactions.withdraw(500, '14/01/2023');
+    expect(statement.printStatement()).toBe('date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00');
   });
 });
